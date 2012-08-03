e0b3c32d8d6df17699b70b1b362dcc90a9afc133
Daniel Tralamazza <tralamazza@gmail.com>
2012-08-03 12:31:43 +0200

    use assertions

--- test/test-dablooms.js
+++ test/test-dablooms.js
@@ -1,8 +1,9 @@
 var dablooms = require('..');
+var assert = require('assert');
 
 var bloom = new dablooms.ScalingBloom(1000, 0.05, '/tmp/bloom.bin', 0);
-console.log("bloom.add('foo', 2) => ", bloom.add('foo', 2));
-console.log("bloom.check('bar') => ", bloom.check('bar'));
-console.log("bloom.remove('foo', 2) => ", bloom.remove('foo', 2));
-console.log("bloom.check('foo') => ", bloom.check('foo'));
-console.log("bloom.flush() => ", bloom.flush());
+assert.equal(bloom.add('foo', 2), 1);
+assert.equal(bloom.check('bar'), 0);
+assert.equal(bloom.remove('foo', 2), 1);
+assert.equal(bloom.check('foo'), 0);
+assert.equal(bloom.flush(), 0);
