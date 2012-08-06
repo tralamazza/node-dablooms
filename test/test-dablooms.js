df462b3215fb4c2f10b29bebfa2f8f3829dadb76
Daniel Tralamazza <tralamazza@gmail.com>
2012-08-06 14:30:42 +0200

    use mocha for testing

--- test/test-dablooms.js
+++ test/test-dablooms.js
@@ -1,12 +1,34 @@
 var dablooms = require('..');
 var assert = require('assert');
 
-var bloom = new dablooms.ScalingBloom(1000, 0.05, '/tmp/bloom.bin', 0);
-assert.equal(bloom.add('foo', 2), 1);
-assert.equal(bloom.check('bar'), 0);
-assert.equal(bloom.error_rate, 0.05);
-assert.equal(bloom.capacity, 1000);
-assert.equal(bloom.remove('foo', 2), 1);
-assert.equal(bloom.check('foo'), 0);
-assert.equal(bloom.flush(), 0);
-assert(bloom.num_bytes > 0);
+describe('Dablooms', function() {
+  describe('ScalingBloom', function() {
+    var bloom = null;
+    before(function() {
+      bloom = new dablooms.ScalingBloom(1000, 0.05, '/tmp/bloom.bin', 0);
+    });
+    it('should have a valid constructor', function() {
+      assert(bloom);
+    });
+    it('should have an error_rate of 0.5', function() {
+      assert.equal(bloom.error_rate, 0.05);
+    });
+    it('should have capacity of 1000', function() {
+      assert.equal(bloom.capacity, 1000);
+    });
+    it('should be able to add a value', function() {
+      assert.equal(bloom.add('foo', 2), 1);
+    });
+    it('check this value membership', function() {
+      assert.equal(bloom.check('foo', 2), 1);
+      assert.equal(bloom.check('bar', 2), 0);
+    });
+    it('should be able to remove a value', function() {
+      assert.equal(bloom.remove('foo', 2), 1);
+      assert.equal(bloom.check('foo', 2), 0);
+    });
+    it('should have a size in bytes larger than zero', function() {
+      assert(bloom.num_bytes > 0);
+    });
+  });
+});
